aea25eeec6dcb629dde286c5982545ffb15b9e8e
firuzsafaev <firuz.safaev@gmail.com>
2025-10-26 18:29:29 +0500

    reqirements.R

---
+++ requirements.R
@@ -0,0 +1,8 @@
+install.packages(c("shiny", "shinydashboard", "rhandsontable",
+                   "data.table", "dplyr", "lubridate",
+                   "shinyalert", "openxlsx", "DBI",
+                   "RPostgres", "httr", "httpuv",
+                   "rmarkdown", "knitr", "jsonlite",
+                   "htmltools", "rsconnect"), 
+                 repos = "https://cloud.r-project.org")
+
