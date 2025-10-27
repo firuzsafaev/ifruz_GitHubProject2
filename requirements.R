c66d94d3d247d2fc23837b45baa1210608874887
firuzsafaev <firuz.safaev@gmail.com>
2025-10-27 21:06:40 +0500

    Remove requirements.R

--- requirements.R
+++
@@ -1,8 +0,0 @@
-install.packages(c("shiny", "shinydashboard", "rhandsontable",
-                   "data.table", "dplyr", "lubridate",
-                   "shinyalert", "openxlsx", "DBI",
-                   "RPostgres", "httr", "httpuv",
-                   "rmarkdown", "knitr", "jsonlite",
-                   "htmltools", "rsconnect"), 
-                 repos = "https://cloud.r-project.org")
-
